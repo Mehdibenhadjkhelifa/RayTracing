f22394229004b1d3ff4143d2352353f3f3157bf7
MehdiBHK <mehdi.bhk13@gmail.com>
2023-10-19 15:14:46 +0100

    Second Episode, Started raytracing!

--- RayTracing/src/WalnutApp.cpp
+++ RayTracing/src/WalnutApp.cpp
@@ -2,8 +2,8 @@
 #include "Walnut/EntryPoint.h"
 
 #include "Walnut/Image.h"
-#include "Walnut/Random.h"
 #include "Walnut/Timer.h"
+#include "Renderer.h"
 using namespace Walnut;
 class ExampleLayer : public Walnut::Layer
 {
@@ -20,8 +20,10 @@ public:
 		ImGui::Begin("ViewPort");
 		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
 		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
-		if(m_Image)
-			ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth() ,(float)m_Image->GetHeight() });
+		auto image = m_Renderer.GetFinalImage();
+		if (image)
+			ImGui::Image(image->GetDescriptorSet(), { (float)image->GetWidth() ,(float)image->GetHeight() },
+				ImVec2(0, 1),ImVec2(1,0));
 		ImGui::End();
 		ImGui::PopStyleVar();
 		Render();
@@ -29,23 +31,14 @@ public:
 
 	void Render(){
 		Timer timer;
+		m_Renderer.OnResize(m_ViewportWidth,m_ViewportHeight);
+		m_Renderer.Render();
 
-		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight()) {
-			m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight,ImageFormat::RGBA);
-			delete[] m_ImageData; 
-			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
-		}
-		for (size_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++) {
-			m_ImageData[i] = Random::UInt();
-			m_ImageData[i] |= 0xff000000;
-		}
-		m_Image->SetData(m_ImageData);
 		m_LastRenderTime = timer.ElapsedMillis();
 	}
 private:
-	std::shared_ptr<Image> m_Image;
+	Renderer m_Renderer;
 	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
-	uint32_t* m_ImageData = nullptr;
 	float m_LastRenderTime = 0;
 };
 
